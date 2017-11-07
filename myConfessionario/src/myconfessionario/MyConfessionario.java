6e52e41d9fe06bb63c997d589b6663169044eb89
ahcar <ahcar@PCdeAlexandre>
2017-11-07 17:37:03 -0200

    Populando o banco de dados

--- myConfessionario/src/myconfessionario/MyConfessionario.java
+++ myConfessionario/src/myconfessionario/MyConfessionario.java
@@ -31,6 +31,7 @@ public class MyConfessionario {
      */
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
         
         
         
